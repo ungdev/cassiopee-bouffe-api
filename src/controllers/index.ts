097e56aef2591a51989a8966259750dccb8d9c75
Thomas de Lachaux <thomas.delachaux@gmail.com>
2021-05-05 09:59:39 +0300

    feat: add swagger

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import yaml from 'yamljs';
+import swagger from 'swagger-ui-express';
 
 import root from './root';
 import vendors from './vendors';
 import callbacks from './callbacks';
+
+const swaggerDocument = yaml.load('openapi.yml');
 
 const router = Router();
 
@@ -11,5 +15,6 @@ router.use(root);
 
 router.use('/vendors', vendors);
 router.use('/callbacks', callbacks);
+router.use('/docs', swagger.serve, swagger.setup(swaggerDocument));
 
 export default router;
